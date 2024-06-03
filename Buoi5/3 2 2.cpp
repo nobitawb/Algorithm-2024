1c3bb93611f888bbebb7c9c67f021b159e8f3267
Ming <167158747+nobitawb@users.noreply.github.com>
2024-06-03 22:22:42 +0700

    Create 3 2 2.cpp

---
+++ Buoi5/3 2 2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n; cin >> n;
+	int max = n;
+	priority_queue <int> b;
+	int a[n];
+	for (int i = 0; i < n; i++){
+		cin >> a[i];
+		b.push(a[i]);
+		while (b.top() == max){
+			cout << b.top() << " ";
+			max--;
+			b.pop();
+		}
+		cout << endl;
+	}
+	
+}
